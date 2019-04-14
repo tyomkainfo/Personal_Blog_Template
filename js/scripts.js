72ae06effb3012da1cfe49ac1bc56156b02db9a2
Shallow96 <oleg_onoprienko@rambler.ru>
2019-04-14 16:10:19 +0300

    Create slider to banner.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,26 @@ $(document).ready(function () {
         $('.animated-icon').toggleClass('open');
     });
 });
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+
+
