58601227833709ffaef05477a01f8efcde5ed4fd
InnaShtepa95 <inna.shtepa@gmail.com>
2019-04-17 18:11:48 +0300

    add form (anim) and button (anim) to contact page

---
+++ js/send.js
@@ -0,0 +1,8 @@
+
+$(".mail-btn").on("click touchstart", function () {
+    $(this).addClass("fly");
+    that = this
+    setTimeout(function() {
+        $(that).removeClass("fly");
+    }, 5400)
+});
