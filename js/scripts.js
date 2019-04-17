51401706b6f0e4f747b47ef2bc39d8e367b8ccad
Shallow96 <oleg_onoprienko@rambler.ru>
2019-04-17 18:43:53 +0300

    Add Like counter to Detail page

--- js/scripts.js
+++ js/scripts.js
@@ -50,3 +50,16 @@ $(document).ready(function(){
 
 
 
+////////////////Like counter////////////////
+var clicks = 2564;
+
+document.getElementById("clicks").innerHTML = clicks;
+
+$('.like-counter').click(function() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+    $('.like-counter').addClass("liked");
+});
+////////////////////////////////////////////
+
+
